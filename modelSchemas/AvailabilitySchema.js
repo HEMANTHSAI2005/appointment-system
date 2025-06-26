402cef9ad46acd3ecf951b00eecb8d8e39857967
HEMANTHSAI2005 <163678484+HEMANTHSAI2005@users.noreply.github.com>
2025-06-26 12:00:16 +0530

    Update AvailabilitySchema.js

--- modelSchemas/AvailabilitySchema.js
+++ modelSchemas/AvailabilitySchema.js
@@ -4,5 +4,5 @@ const availabilitySchema = new mongoose.Schema({
   professor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   slots: [{ type: String, required: true }] 
 });                                                     
-
+          
 export default mongoose.model('Availability', availabilitySchema);      
